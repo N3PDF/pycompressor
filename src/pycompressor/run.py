b939c51ae737f7e66e0d8823d3a00218470c3d9b
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2020-06-02 15:07:36 +0200

    Modified codes for minimization

--- src/pycompressor/run.py
+++ src/pycompressor/run.py
@@ -5,10 +5,11 @@ Main file for Compressor
 # import os
 # import logging
 import argparse
+from tqdm import trange
 
 from pycompressor.pdf_grid import XGrid
 from pycompressor.pdf_grid import PdfSet
-from pycompressor.compressor import error_function
+from pycompressor.compressor import compress
 
 
 def positive_int(value):
@@ -72,10 +73,40 @@ def main():
     else:
         q0 = args.qinit
 
+    # List of estimators
+    est_dic = {
+            'moment_estimators': [
+                'mean',
+                'stdev',
+                'skewness',
+                'kurtosis'
+                ],
+            'stat_estimators': [
+                'kolmogorov_smirnov'
+                ]
+            }
+
+    # GA parameters
+    ga_params = {
+            'number_mutation': 5
+            }
+
     # Construc xgrid
+    print('[+] Loading PDF set:')
     xgrid = XGrid().build_xgrid()
     prior = PdfSet(pdf, xgrid, q0, nfl).build_pdf()
 
-    # Compute error function
-    erf_tot = error_function(prior, nbr)
-    print(f"Total ERF: {erf_tot}")
+    # Init. compressor class
+    comp = compress(prior, est_dic, nbr)
+
+    # Run compressor
+    nb_iter = 15000
+    print('\n[+] Compressing replicas:')
+    with trange(nb_iter) as iter_range:
+        for i in iter_range:
+            iter_range.set_description("Compression")
+            erf, index = comp.genetic_algorithm(ga_params)
+            iter_range.set_postfix(ERF=erf)
+
+    # Fetching ERF and construct reduced PDF grid
+    print(f'\n[+] Final ERF:{erf}\n')

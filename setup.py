916433412b88a6bcd561f383a15448f868790798
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2020-06-02 15:06:12 +0200

    Added more information

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+"""
+pyCompressor: Python implementation of compressor presented
+in this paper https://arxiv.org/pdf/1504.06469 and implemented
+here https://github.com/scarrazza/compressor.
+
+This program has been developed within the framework of the
+N3PDF group n3pdf.mi.infn.it/
+
+Authors: - Tanjona R. Rabemananjara
+         - Juan Cruz Martinez
+         - Stefano Carrazza
+
+License: GPL-3.0
+"""
+
+
 import os
 import re
 import sys
@@ -6,17 +22,28 @@ from setuptools import find_packages
 
 PACKAGE = "pycompressor"
 
-requirements = ["numpy", "scipy", "matplotlib"]
+requirements = ["numpy", "tqdm", "scipy", "matplotlib"]
 
+# Check python version
 if sys.version_info < (3, 6):
     print("cyclejet requires Python 3.6 or later", file=sys.stderr)
     sys.exit(1)
 
+# Check if LHAPDF is installed
+try:
+    import lhapdf
+except ImportError:
+    print(f"Note: {PACKAGE} requires the installation of LHAPDF")
 
-with open("README.md") as f:
-    long_desc = f.read()
+# Read through Readme
+try:
+    with open("README.md") as f:
+        long_desc = f.read()
+except IOError:
+    print("Read me file not found")
 
 
+# Get package version
 def get_version():
     """
     Gets the version from the package's __init__ file
@@ -35,8 +62,8 @@ setup(
     name=PACKAGE,
     version=get_version(),
     description="PDF set compressor",
-    author="",
-    author_email="",
+    author="Tanjona R. Rabemananjara, Juan Cruz Martinez, Stefano Carrazza",
+    author_email="tanjona.rabemananjara@mi.infn.it",
     url="https://github.com/N3PDF/pyCompressor",
     long_description=long_desc,
     install_requires=requirements,
@@ -52,9 +79,3 @@ setup(
         "Topic :: Scientific/Engineering :: Physics",
     ],
 )
-
-# Check if LHAPDF is installed
-try:
-    import lhapdf
-except ImportError:
-    print(f"Note: {PACKAGE} requires the installation of LHAPDF")

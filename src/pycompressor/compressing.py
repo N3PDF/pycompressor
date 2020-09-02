847e919c65072dda3705d59d05e4d3e6d57c38ef
Tanjona R <tanjona.rabemananjara@unimi.It>
2020-09-02 22:43:47 +0200

    Implement GANs interface

--- src/pycompressor/compressing.py
+++ src/pycompressor/compressing.py
@@ -1,9 +1,11 @@
 import json
+import shutil
 import logging
 import pathlib
 import numpy as np
-from tqdm import trange
+import subprocess as sub
 
+from tqdm import trange
 from pycompressor.pdfgrid import XGrid
 from pycompressor.pdfgrid import PdfSet
 from pycompressor.compressor import compress
@@ -16,40 +18,64 @@ Q0 = 1
 NF = 3
 
 
-def compressing(fit, compressed, minimizer, est_dic):
+def compressing(pdf, compressed, minimizer, est_dic, enhance, nbgen):
     """
     Action that performs the compression. The parameters
     for the compression are provided by a `runcard.yml`.
 
     Parameters
     ----------
-        fit: str
-            Fit/PDF name
+        pdf: str
+            pdf/PDF name
         compressed: int
             Size of the compressed set
         est_dic: dict
             Dictionary containing the list of estimators
     """
 
-    pdf = str(fit)
+    if not enhance:
+        pdf = str(pdf)
+    else:
+        from pycompressor.postgans import postgans
+        # Enhance with GANs
+        outfolder = str(pdf) + "_enhanced"
+        sub.call(
+            [
+                "ganpdfs",
+                "runcard.yml",
+                "-o",
+                f"{outfolder}",
+                "-k",
+                f"{nbgen}",
+                "--force",
+            ]
+        )
+        # Evolve Generated Grids
+        shutil.copy("filter.yml", f"{outfolder}/filter.yml")
+        sub.call(["evolven3fit", f"{outfolder}", f"{nbgen}"])
+        # Add symbolic Links to LHAPDF dataDir
+        postgans(str(pdf), outfolder, nbgen)
+        pdf = str(pdf) + "_enhanced"
+
     # Create output folder
     folder = pathlib.Path().absolute() / pdf
     folder.mkdir(exist_ok=True)
     # Create output folder for ERF stats
     out_folder = pathlib.Path().absolute() / "erfs_output"
     out_folder.mkdir(exist_ok=True)
-    print("[+] Loading PDF set:")
+    log.info("Loading PDF set:")
     xgrid = XGrid().build_xgrid()
     prior = PdfSet(pdf, xgrid, Q0, NF).build_pdf()
     # Set seed
     np.random.seed(0)
+
     # Init. compressor class
     comp = compress(prior, est_dic, compressed, out_folder)
     # Start compression depending on the Evolution Strategy
     erf_list = []
     final_result = {"pdfset_name": pdf}
 
-    print(f"\n[+] Compressing replicas using {minimizer} algorithm:")
+    log.info(f"\nCompressing replicas using {minimizer} algorithm:")
     if minimizer == "genetic":
         # Run compressor using GA
         nb_iter = 15000
@@ -74,7 +100,7 @@ def compressing(fit, compressed, minimizer, est_dic):
         json.dump(final_result, outfile)
 
     # Fetching ERF and construct reduced PDF grid
-    print(f"\n[+] Final ERF: {erf}\n")
+    log.info(f"\nFinal ERF: {erf}\n")
 
     # Compute final ERFs for the final choosen replicas
     final_err_func = comp.final_erfs(index)

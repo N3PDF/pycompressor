191a900d15e2e24d0c96f8cf7a1083b1e7d2bfed
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2020-12-31 12:27:08 +0100

    Add KL test on distributions along x

--- tools/compare_distributions.py
+++ tools/compare_distributions.py
@@ -7,6 +7,7 @@ import pathlib
 import argparse
 import numpy as np
 from numba import njit
+from scipy import stats
 from scipy import interpolate
 from scipy.linalg import sqrtm
 import matplotlib.pyplot as plt
@@ -51,13 +52,19 @@ def posint(value):
     return ivalue
 
 
-def plot_dists_per_fl(x, prior_fl, stand_fl, enhcd_fl, info):
+def plot_dists_per_fl(x, prior_fl, stand_fl, enhcd_fl, info, fit):
     plotName = info["plotName"]
     fig, axes = plt.subplots(ncols=2, nrows=3, figsize=(20, 18))
     xind = [5, 10, 15, 45, 55, 68]
     for i, axis in enumerate(axes.reshape(-1)):
-        axis.hist(
-            prior_fl[:,xind[i]],
+        pprior = prior_fl[:,xind[i]]
+        sstand = stand_fl[:,xind[i]]
+        eenhcd = enhcd_fl[:,xind[i]]
+        mp, sp = stats.norm.fit(pprior)
+        ms, ss = stats.norm.fit(sstand)
+        me, se = stats.norm.fit(eenhcd)
+        _, binp, _ = axis.hist(
+            pprior,
             histtype="step",
             bins=info.get("bins", 20),
             color="green",
@@ -66,39 +73,55 @@ def plot_dists_per_fl(x, prior_fl, stand_fl, enhcd_fl, info):
             linewidth=2.5,
             density=True
         )
+
+        # Prior
+        pmin, pmax = min(binp), max(binp)
+        lnp = np.linspace(pmin, pmax, len(pprior))
+        pfit = stats.norm.pdf(lnp, mp, sp)  # Prior
+        sfit = stats.norm.pdf(lnp, ms, ss)  # Standard
+        efit = stats.norm.pdf(lnp, me, se)  # Enhanced
+        # Compute KL ests
+        kl_stand = stats.ks_2samp(pprior, sstand, mode="auto")
+        kl_enhcd = stats.ks_2samp(pprior, eenhcd, mode="auto")
+
         axis.hist(
-            stand_fl[:,xind[i]],
+            sstand,
             histtype="step",
             bins=info.get("bins", 20),
             color="deeppink",
             alpha=1,
-            label="Standard",
+            label=f"Standard  KL(v={kl_stand[0]:.4f}, p={kl_stand[1]:.4f})",
             linewidth=2.5,
             density=True
         )
         axis.hist(
-            enhcd_fl[:,xind[i]],
+            eenhcd,
             histtype="step",
             bins=info.get("bins", 20),
             color="dodgerblue",
             alpha=1,
-            label="Enhanced",
+            label=f"Enhanced KL(v={kl_enhcd[0]:.4f}, p={kl_enhcd[1]:.4f})",
             linewidth=2.5,
             density=True
         )
+        if fit:
+            # Plot fits
+            axis.plot(lnP, pfit, "--", color="green", linewidth=2)
+            axis.plot(lnP, sfit, "--", color="deeppink", linewidth=2)
+            axis.plot(lnP, efit, "--", color="dodgerblue", linewidth=2)
         # Params & Info
         # axis.set_xlabel(info["xlabel"])
         # axis.set_ylabel(info["ylabel"])
-        axis.set_title(f"x={x[xind[i]]}")
+        axis.set_title(f"x={x[xind[i]]:.4f}", fontsize=16)
         axis.grid(alpha=0.1, linewidth=1.5)
         axis.tick_params(length=7, width=1.5)
-        axis.legend()
+        axis.legend(fontsize=14)
     fig.suptitle(info["figTitle"])
     fig.savefig(f"distributions/{plotName}.png", dpi=250)
     plt.close("all")
 
 
-def plot_dists(x, prior, stand, ehncd, bins=10):
+def plot_dists(x, prior, stand, ehncd, bins=10, fit=False):
     info = {
         "bins": bins,
         "xlabel": "x",
@@ -109,7 +132,7 @@ def plot_dists(x, prior, stand, ehncd, bins=10):
     for fl in range(prior.shape[0]):
         info["figTitle"] = IDS[fl]
         info["plotName"] = f"dist_{IDS[fl]}"
-        plot_dists_per_fl(x, prior[fl], stand[fl], ehncd[fl], info)
+        plot_dists_per_fl(x, prior[fl], stand[fl], ehncd[fl], info, fit)
 
 
 def arg_parser():

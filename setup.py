b4af21ac56b613ebaff2cdb93a6b7359e5cc10b6
Tanjona R <tanjona.rabemananjara@unimi.It>
2021-01-26 23:38:34 +0100

    Bump version: 0.1.0 → 1.0.0-dev

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except ImportError:
 
 setup(
     name=PACKAGE,
-    version='0.1.0',
+    version='1.0.0-dev',
     description="PDF Compression",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
